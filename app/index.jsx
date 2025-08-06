c71347ff70c98f8b242cf3811d725320806e735e
ccqsantos <caua0.cicarone@gmail.com>
2025-08-06 16:22:31 -0300

    add selectPdf function and button

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,25 @@
-import { SafeAreaView, Text, StyleSheet, Pressable, View } from "react-native";
+import { SafeAreaView, Text, StyleSheet, Pressable, View, ActivityIndicator, Button } from "react-native";
 import { Link } from "expo-router";
+import React, { useState, useEffect } from 'react';
+import * as DocumentPicker from "expo-document-picker";
+
 
 export default function Home() {
+    const [selectedPdf, setselectedPdf] = useState(null); //cria um async do estado de selecao do pdf
+    
+    async function pickDocument(){ //abre o pdf picker
+        let result = await DocumentPicker.getDocumentAsync({
+            type: "application/pdf",
+            copyToCacheDirectory: true
+        })
+
+        if(result.assets && result.assets[0].uri){ //pega apenas o primeiro pdf selecionado, por isso posicao 0
+            console.log(result.assets[0].uri);
+            setselectedPdf(result.assets[0].uri); //passa o pdf para a funcao useState que atualiza o app
+        }//AINDA NAO DÁ PRA VISUALIZAR O ARQUIVO!!!
+    }
+    
+    
     return (
         <SafeAreaView style={styles.safeContainer}>
             <View style={styles.container}>
@@ -9,6 +27,8 @@ export default function Home() {
 
                 <Text>Artigo 1</Text>
                 <Text>Artigo 2</Text>
+
+                <Button title="Escolher PDF" onPress={pickDocument}/>
 
                 <Link href="/login" asChild>
                     <Pressable style={{ padding: 10, backgroundColor: 'blue' }}>
